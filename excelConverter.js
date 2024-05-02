a0bcf465e42fb4148198587b50903699b8ac7992
Igor <imitroshichev@gmail.com>
2024-05-02 13:04:17 +0300

    first commit

--- excelConverter.js
+++ excelConverter.js
@@ -9,16 +9,48 @@ async function convertExcelToXmlAndSave(filePath) {
         const workbook = xlsx.readFile(filePath);
         const sheetName = workbook.SheetNames[0];
         const sheet = workbook.Sheets[sheetName];
-        const jsonData = xlsx.utils.sheet_to_json(sheet);
-        const root = xmlbuilder.create('TgMall');
 
-        jsonData.forEach(row => {
+        // Создаем пустой объект для хранения данных столбцов
+        const columnValues = {};
+
+        // Проходимся по каждой ячейке в листе Excel
+        const range = xlsx.utils.decode_range(sheet['!ref']);
+        for (let R = range.s.r; R <= range.e.r; ++R) {
+            for (let C = range.s.c; C <= range.e.c; ++C) {
+                const cellAddress = { c: C, r: R };
+                const cellRef = xlsx.utils.encode_cell(cellAddress);
+                const cell = sheet[cellRef];
+                if (cell) {
+                    // Получаем имя столбца и имя колонки
+                    const columnName = xlsx.utils.encode_col(C);
+                    const columnHeader = cell.v;
+
+                    // Извлекаем значение ячейки
+                    const cellValue = cell.v;
+
+                    // Инициализируем массив значений для столбца, если это первый раз встречаем столбец
+                    if (!columnValues[columnName]) {
+                        columnValues[columnName] = { column: columnHeader, values: [] };
+                    }
+                    // Добавляем значение в массив для данного столбца
+                    columnValues[columnName].values.push(cellValue);
+                }
+            }
+        }
+
+        // Создаем XML
+        const root = xmlbuilder.create('TgMall');
+        const columnNames = Object.keys(columnValues);
+        const rowCount = columnValues[columnNames[0]].values.length;
+        for (let i = 0; i < rowCount; i++) {
+            if (i === 0) continue; // Пропускаем первую итерацию для пропуска первого тега <products> с описанием
             const entry = root.ele('products');
-            Object.keys(row).forEach(key => {
-                const englishKey = key.replace(/[^a-zA-Z0-9]/g, '_');
-                entry.ele(englishKey, row[key]);
+            columnNames.forEach(columnName => {
+                const value = columnValues[columnName].values[i];
+                const columnHeader = columnValues[columnName].column;
+                entry.ele(columnHeader, value);
             });
-        });
+        }
 
         const xmlString = root.end({ pretty: true });
         const xmlFileName = `${Date.now()}_converted.xml`;
